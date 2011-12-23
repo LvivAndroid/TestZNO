b8fb430d78b11831cdf43baaaa524386588d81fd
Andrew Kharchyshyn <haawaplus@gmail.com>
2011-12-23 12:47:40 +0200

    haawa rules the world.

---
+++ Project/src/com/h/h/h/Menu.java
@@ -0,0 +1,49 @@
+package com.h.h.h;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class Menu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        
+        
+       
+    }
+
+    @Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		// TODO Auto-generated method stub
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.optionmenu,menu);
+	    return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		 // Handle item selection
+	    switch (item.getItemId()) {
+	    case R.id.SettingItem:
+	    	return true;
+	        
+	    case R.id.AboutItem:
+	    
+	        finish();
+	        return true;
+	    
+	    default:
+		
+		return super.onOptionsItemSelected(item);
+	    }
+		
+	}
+    
+    
+    
+}
---
+++ Project/src/com/h/h/h/Settings.java
@@ -0,0 +1,22 @@
+package com.h.h.h;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Settings extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+		
+		
+		
+		
+	}
+
+	
+	
+	
+}
